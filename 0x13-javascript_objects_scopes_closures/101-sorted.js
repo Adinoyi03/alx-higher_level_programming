08349139fe1c7d7d39e3b5558692ab88e95c1a05
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2023-03-14 13:26:46 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-let dict = require('./101-data').dict;
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
 
-let storage = {};
-
-for (let item in dict) {
-  if (!storage[dict[item]]) {
-    storage[dict[item]] = [];
-  }
-  storage[dict[item]].push(item);
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
 }
-
-console.log(storage);
+console.log(Dictn);
