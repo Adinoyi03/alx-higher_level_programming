2bc0d6d0f6c056177ee004364ec85963fbe4ea5a
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2023-04-26 00:57:57 +0100

    Update file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
-Footer
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+});
