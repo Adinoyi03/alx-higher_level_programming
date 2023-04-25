7c2cd16cd54f34363241e651edff31749b69b47a
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2023-04-26 00:40:52 +0100

    Update file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let i = 0; i < json.length; i++) {
-      if (json[i].completed === true) {
-        if (resp[json[i].userId] === undefined) {
-          resp[json[i].userId] = 0;
-        }
-        resp[json[i].userId]++;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(resp);
+    });
+    console.log(completed);
   }
 });
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
