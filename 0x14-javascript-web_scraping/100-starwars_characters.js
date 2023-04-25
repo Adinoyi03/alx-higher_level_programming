d2558dd3c28e38a82235098b580bc43c54ce78e2
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2023-04-26 00:54:45 +0100

    Update 100-file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
+// Use  Star wars API to get
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
+    for (const character of characters) {
+      // console.log(character);
       request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+        if (!error && response.statusCode === 200) {
+          const info = JSON.parse(body);
+          console.log(info.name);
         }
       });
-    });
+    }
   }
-});
+}
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], callback);
