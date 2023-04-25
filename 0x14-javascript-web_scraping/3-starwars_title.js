e3197bb0b2afa3a47c8c151737746506e4ce8469
Muhammed Abdulrahaman Adinoyi <adinoyi360@gmail.com>
2023-04-26 00:59:36 +0100

    Update file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
+// Use  Star wars API to get
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    console.log(info.title);
+  }
+}
+request(options, callback);
